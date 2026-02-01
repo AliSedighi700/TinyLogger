be04a2c52b873abdae97edb57541eb4d694c54f6
alis <asadighi700@gmail.com>
2026-02-02 00:43:20 +0100

    Parsed the price

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ bool download_price(const std::string& coin_id,
 
     std::string cmd = "curl -s -o \"" + outfile + "\" \"" + url + "\"";
     std::cout << "Fetching " << coin_id << "...\n";
+    std::cout << "===============================" << '\n';
     std::cout << "Command " <<  cmd << '\n';
 
     int ret = std::system(cmd.c_str());
@@ -35,7 +36,8 @@ bool download_price(const std::string& coin_id,
     if(std::filesystem::exists(outfile) &&
        std::filesystem::file_size(outfile) > 10)
     {
-        std::cout << "Success " << outfile << " created (" << std::filesystem::file_size(outfile) << ")[bytes] \n\n";
+        std::cout << "Success: " << outfile << " created. "
+            << std::filesystem::file_size(outfile) << " [bytes] \n\n";
         return true;
     }
 
@@ -43,17 +45,60 @@ bool download_price(const std::string& coin_id,
     return false;
 }
 
+double parse_bitcoin_usd_price(const std::string& filename = "temp.json")
+{
+    std::ifstream file(filename);
+    if(!file.is_open())
+    {
+        std::cerr << "Cannot open "<< filename << '\n';
+    }
+    std::string json((std::istreambuf_iterator<char>(file)),
+                     std::istreambuf_iterator<char>());
+    file.close();
+    size_t pos = json.find("\"usd\":");
+    if(pos == std::string::npos)
+    {
+        std::cerr << "Could not find \"usd\": in JASON\n";
+        return -1.0;
+    }
+
+    const std::string key = "\"usd\":";
+    pos += key.size();
+
+    while(pos < json.size() && std::isspace(json[pos])) ++pos;
+    size_t end = pos;
+    while(end < json.size() && (std::isdigit(json[end]) || json[end] == '.' )) ++end;
+
+    if(end == pos)
+    {
+        std::cerr << "No number founded after \"usd\":\n";
+        return -1.0;
+    }
+
+    std::string price_str = json.substr(pos, end - pos);
+    try
+    {
+        double price = std::stod(price_str);
+        return price;
+    }catch(...)
+    {
+        std::cerr << "Failed to convert " << price_str << " to double" << '\n';
+    }
+    return 0;
+}
+
 int main()
 {
     std::cout << "concurrent fetcher and arbitrage checker" << '\n';
 
-    bool ok = download_price("bitcoin");
+    double price = parse_bitcoin_usd_price();
 
-    if(ok)
+    if(price > 0.0)
     {
         std::cout << "market data is downloaded" << '\n';
+        std::cout << "Current bitcon price: $" << price << '\n';
     }else{
-        std::cout << "download failed" << '\n';
+        std::cout << "Parsing failed" << '\n';
     }
 
 
